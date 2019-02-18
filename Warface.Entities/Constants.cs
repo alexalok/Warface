79dae74e9f8269ea3d0be96afa4192debc4f6895
alexalok <contact@alexalok.me>
2019-02-18 23:58:46 +0300

    Add MaxSafeMessageBoxLength constant

--- Warface.Entities/Constants.cs
+++ Warface.Entities/Constants.cs
@@ -2,10 +2,11 @@
 {
     public class Constants
     {
-        public const int MinNicknameLength = 4;
-        public const int MaxNicknameLength = 24;
-        public const int RmLeagueCount     = 22;
-        public const int TokenLength       = 64;
-        public const int MaxAnnounceLength = 300;
+        public const int MinNicknameLength       = 4;
+        public const int MaxNicknameLength       = 24;
+        public const int RmLeagueCount           = 22;
+        public const int TokenLength             = 64;
+        public const int MaxAnnounceLength       = 300;
+        public const int MaxSafeMessageBoxLength = 150;
     }
 }
