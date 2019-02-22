75235c8e1b8aa54ad3a4d3b4cc1d0ad9b78b7b7e
alexalok <contact@alexalok.me>
2019-02-22 22:48:07 +0300

    Add image to GameroomMission

--- Warface.Entities/Gamerooms/GameroomMission.cs
+++ Warface.Entities/Gamerooms/GameroomMission.cs
@@ -6,12 +6,13 @@ namespace Warface.Entities.Gamerooms
 {
     public struct GameroomMission
     {
-        public string Key { get; }
-        public string Mode { get; }
-        public string Type { get; }
-        public string ModeName { get; }
-        public string Name { get; }
+        public string     Key        { get; }
+        public string     Mode       { get; }
+        public string     Type       { get; }
+        public string     ModeName   { get; }
+        public string     Name       { get; }
         public Difficulty Difficulty { get; }
+        public string     Image      { get; }
 
         //[CanBeNull]
         //public string GetFriendlyName() //null if mission list is not loaded or pve refreshed
@@ -20,14 +21,15 @@ namespace Warface.Entities.Gamerooms
         //    return _missionsController.List.FirstOrDefault(m => m.Key == key)?.Type;
         //}
 
-        GameroomMission(string key, string mode, string type, string modeName, string name, Difficulty difficulty)
+        GameroomMission(string key, string mode, string type, string modeName, string name, Difficulty difficulty, string image)
         {
-            Key = key;
-            Mode = mode;
-            Type = type;
-            ModeName = modeName;
-            Name = name;
+            Key        = key;
+            Mode       = mode;
+            Type       = type;
+            ModeName   = modeName;
+            Name       = name;
             Difficulty = difficulty;
+            Image      = image;
         }
 
         public static GameroomMission ParseNode(HtmlNode gameroomMissionNode)
@@ -36,14 +38,15 @@ namespace Warface.Entities.Gamerooms
              * setting='pvp/stm_wharf' mode='stm' mode_name='@pvp_stm_game_mode_desc' mode_icon='tdm_icon' 
              * description='@pvp_stm_mission_desc' image='mapImgStmWharf' difficulty='normal' type='' time_of_day='09.30' revision='668'>*/
 
-            string key = gameroomMissionNode.Attributes["mission_key"].Value;
-            string mode = gameroomMissionNode.Attributes["mode"].Value;
+            string key      = gameroomMissionNode.Attributes["mission_key"].Value;
+            string mode     = gameroomMissionNode.Attributes["mode"].Value;
             string modeName = gameroomMissionNode.Attributes["mode_name"].Value;
-            string name = gameroomMissionNode.Attributes["name"].Value;
+            string name     = gameroomMissionNode.Attributes["name"].Value;
             Enum.TryParse(gameroomMissionNode.Attributes["name"].Value, true, out Difficulty difficulty);
-            string type = gameroomMissionNode.Attributes["type"].Value;
+            string type  = gameroomMissionNode.Attributes["type"].Value;
+            string image = gameroomMissionNode.Attributes["image"].Value;
 
-            return new GameroomMission(key, mode, type, modeName, name, difficulty);
+            return new GameroomMission(key, mode, type, modeName, name, difficulty, image);
         }
     }
 }
