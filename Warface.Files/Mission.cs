7e2fde54ab11dc5639e04c8dd4ae1daaf22ef808
alexalok <contact@alexalok.me>
2019-03-21 20:56:06 +0300

    Add Mission game object parser

---
+++ Warface.Files/Mission.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using HtmlAgilityExtended;
+using HtmlAgilityPack;
+
+namespace Warface.Files
+{
+    public class Mission
+    {
+        public string Name { get; }
+
+        public Guid Uid { get; }
+
+        public bool ReleaseMission { get; }
+
+        public string DescriptionIcon { get; }
+
+        Mission(string name, Guid uid, bool releaseMission, string descriptionIcon)
+        {
+            Name            = name;
+            Uid             = uid;
+            ReleaseMission  = releaseMission;
+            DescriptionIcon = descriptionIcon;
+        }
+
+        public static Mission ParseNode(HtmlNode missionNode)
+        {
+            string name   = missionNode.Attributes["name"].Value;
+            string uidStr = missionNode.Attributes["uid"].Value;
+            var    uid    = Guid.Parse(uidStr);
+            bool   releaseMission; //Blackwood Games strikes again!
+            if (missionNode.Attributes.Contains("release_mission"))
+                releaseMission = missionNode.Attributes["release_mission"].BoolValue();
+            else if (missionNode.Attributes.Contains("test_mission"))
+                releaseMission = !missionNode.Attributes["test_mission"].BoolValue();
+            else
+                throw new NotSupportedException();
+            string icon = missionNode.SelectSingleNode("./ui/description").Attributes["icon"].Value;
+            return new Mission(name, uid, releaseMission, icon);
+        }
+    }
+}
