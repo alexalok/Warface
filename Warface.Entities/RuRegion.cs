daf6a592dbf07cbd5ee195cf1114748376c854b7
alexalok <contact@alexalok.me>
2019-03-21 20:55:39 +0300

    Add RuRegion enum

---
+++ Warface.Entities/RuRegion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Warface.Entities
+{
+    public enum RuRegion
+    {
+        Global,
+        Krasnodar,
+        Novosibirsk,
+        Khabarovsk
+    }
+
+    public static class RuRegionExtensions
+    {
+        public static string ToFriendlyString(this RuRegion region)
+        {
+            switch (region)
+            {
+                case RuRegion.Global:
+                    return "Москва";
+                case RuRegion.Krasnodar:
+                    return "Краснодар";
+                case RuRegion.Novosibirsk:
+                    return "Новосибирск";
+                case RuRegion.Khabarovsk:
+                    return "Хабаровск";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(region), region, null);
+            }
+        }
+    }
+}
